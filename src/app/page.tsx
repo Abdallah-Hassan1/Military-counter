29d60b6998cc3c38eecf01e33a52a3f8f1fd1dfe
Abdallah Hassan <engabdallahhassan6@gmail.com>
2026-02-01 20:40:32 +0200

    adit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import Time from "@/app/time";
 import { useState } from "react";
 
 export default function Home() {
+  const [years, setYears] = useState(0);
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [finished, setFinished] = useState(false); // after the time is over, change the default value to "true"
-  const date = "Jun 1, 2026";
+  const date = "May 25, 2026";
   const endDate = new Date(date).getTime();
 
   // Update the count down every 1 second
@@ -26,9 +27,15 @@ export default function Home() {
 
     distance = distance < 0 ? -distance : distance;
 
+    const milliSecondsOfDay = 86400000;
     // Time calculations for days, hours, minutes and seconds
-    setDays(Math.floor(distance / (1000 * 60 * 60 * 24)));
-    setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    setYears(Math.floor(distance / (milliSecondsOfDay * 365)));
+    setDays(
+      Math.floor(
+        (distance % (milliSecondsOfDay * 365)) / (milliSecondsOfDay),
+      ),
+    );
+    setHours(Math.floor((distance % (milliSecondsOfDay)) / (1000 * 60 * 60)));
     setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
     setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
   }, 1000);
@@ -49,7 +56,10 @@ export default function Home() {
           <span className="text-gray-300">Security Forces</span>
         </div>
         <div className="mt-10 -mb-50">
-          End date: <span className=" text-red-500">{date}</span>
+          End date:{" "}
+          <span className={`${finished ? "text-green-500" : "text-red-500"}`}>
+            {date}
+          </span>
         </div>
       </div>
 
@@ -59,10 +69,11 @@ export default function Home() {
         </div>
       )}
       <div className="flex flex-row flex-wrap items-center justify-center mt-60">
+        {finished && <Time number={years} type={"years"} />}
         <Time number={days} type={"days"} />
         <Time number={hours} type={"hours"} />
         <Time number={minutes} type={"minutes"} />
-        <Time number={seconds} type={"seconds"} />
+        {!finished && <Time number={seconds} type={"seconds"} />}
       </div>
     </div>
   );
